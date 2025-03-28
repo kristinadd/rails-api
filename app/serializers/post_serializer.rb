36822d68e9841a390af69d85dc74a9b63c73494f
kristinadd <kristina@productboard.com>
2025-03-28 10:03:25 +0100

    V1

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,9 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :created_at, :published
+  # no need to include id, it' handled automatically by ActiveModel::Serializer
+  # and it will be included in the JSON response
+  # by default
+  
+  attributes :title, :content, :created_at, :published
+
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_28_055208) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_28_081601) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.boolean "published"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "posts", "users"
 end
