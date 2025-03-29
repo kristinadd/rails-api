5d2c62ba0c0471c6962b5713468e4d2aed91aa77
kristinadd <kristina@productboard.com>
2025-03-29 16:43:59 +0100

    rmv id from the top-level hash

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,9 +1,10 @@
 class PostSerializer < ActiveModel::Serializer
-  # no need to include id, it' handled automatically by ActiveModel::Serializer
-  # and it will be included in the JSON response
-  # by default
-  
   attributes :title, :content, :created_at, :published
 
   belongs_to :user
+
+  def serializable_hash(adapter_options = nil, options = {}, adapter_instance = self.class.serialization_adapter_instance)
+    hash = super
+    hash.except(:id) # remove :id from the top-level hash
+  end
 end
